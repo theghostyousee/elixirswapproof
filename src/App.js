b7666618c001da1e413816e9ddf5a357aa807011
theghostyousee <patrickserise174@gmail.com>
2023-04-04 18:08:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Logo from './logo.svg';
 
 function App() {
+
+  function OpenUrl(){
+    window.open("https://twitter.com/intent/tweet?text=I%27m%20verifying%20my%20ability%20in%20the%20science%20of%20potions%20to%20become%20a%20beta%20tester%20of%20%40SwapPotion.%20%F0%9F%A7%AA&")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <img src={Logo}></img>
+      <div className='buff'>
+
+        <div className="bruv">
+          <h4>Lending the next wave of interoperability</h4>
+          <h1>An Omnichain </h1>
+          <h1>Liquidity Platform</h1>
+          <button onClick={OpenUrl}>Apply for Beta</button>
+        </div>
+
+
+      </div>
+
+      
     </div>
   );
 }
